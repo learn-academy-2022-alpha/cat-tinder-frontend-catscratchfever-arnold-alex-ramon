36882d6ff5ba2995cd2a001884bd5e91b42656ac
Arnold Laoang <arnoldlaoangjr@gmail.com>
2022-04-07 10:53:39 -0700

    Resolved issue

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Enzyme from 'enzyme'
 
 ReactDOM.render(
   <React.StrictMode>
